1bc64cca7cf70466b551612c49a1ae71423773e2
nameless0422 <cjy042218@gmail.com>
2026-03-03 08:50:24 +0900

    .

---
+++ src/test/java/nameless0422/stock_management_api/StockManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nameless0422.stock_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
